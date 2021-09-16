987095b7c6e4a9b0ec25933acc85dcae1a91a85b
Isra Elsharef <isra.elsharef@accenture.com>
2021-09-16 20:30:45 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.myresourcegroup.location
   address_space       = [var.address_space]
   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  Department          = "devops"
 }
 
 resource "azurerm_subnet" "subnet" {
